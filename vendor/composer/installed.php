31078a3a96b15ecbd7c334fc2744b576a47fe8ca
Julio Abreu CanalDev33 <canaldev33@gmail.com>
2025-07-28 15:43:30 -0300

    Resolve conflitos após pull

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '8b6f431b1e3f149f07c27933b4ce3c578ed3857c',
         'name' => 'seu-usuario/sistema_monitoramento',
         'dev' => true,
     ),
     'versions' => array(
         'seu-usuario/sistema_monitoramento' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '8b6f431b1e3f149f07c27933b4ce3c578ed3857c',
             'dev_requirement' => false,
         ),
     ),
